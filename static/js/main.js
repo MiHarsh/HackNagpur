4f6f8babd21460114d729a66c4e1d605c9a567b4
sonali522 <76245085+sonali522@users.noreply.github.com>
2020-12-29 19:41:28 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    // Init
+    $('.image-section').hide();
+    $('.loader').hide();
+    $('#result').hide();
+
+    // Upload Preview
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').hide();
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function () {
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        $('#result').hide();
+        readURL(this);
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                $('.loader').hide();
+              //  $('#result').fadeIn(600);
+                //$('#result').text(' Result:  ' + data);
+       var header = $('#results thead');
+       var body = $('#results tbody');
+       
+       // Headers
+       var keyList = Object.keys(data);
+       
+       // Body
+       for (var h = 0; h < keyList.length; h++) {
+          var d = data[keyList[h]];
+          var hTr; 
+       $('#results tbody').append(hTr = $('<tr>'));
+          hTr.append($('<td>', { text: keyList[h] }));
+          hTr.append($('<td>', { text: d }));
+            
+       }
+                $('#results').show();
+                $('#description').show();
+                console.log('Success!');
+            },
+        });
+    });
+
+});
